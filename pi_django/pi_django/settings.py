1b8e41a887956424663b86f1adfc74fe8265f223
Miguel Correia <miguelluiscorreia@ua.pt>
2019-06-11 02:13:18 +0100

    settings change

--- pi_django/pi_django/settings.py
+++ pi_django/pi_django/settings.py
@@ -139,5 +139,12 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated', )
 }
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+
 # fix phone reqs
 #APPEND_SLASH = False

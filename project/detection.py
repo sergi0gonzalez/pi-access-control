0bb5fc966753fce5cfd908e2c2c7e49742602eaa
mirecki <michal.mirecki95@gmail.com>
2019-06-11 16:01:25 +0100

    face & blink detection

--- project/detection.py
+++ project/detection.py
@@ -93,10 +93,10 @@ while True:
     # Display the results
     for (top, right, bottom, left), name in zip(face_locations, face_names):
         # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-        top *= 4
-        right *= 4
-        bottom *= 4
-        left *= 4
+        top = 4 * top
+        right = 4 * right
+        bottom= 4 * bottom
+        left = 4 * left
 
         # Draw a box around the face
         cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
